350ee9459a2de7fb17b915e6ccaace6125747efd
Felipe Gutierrez <felipe.gutierrezcruz@cockroachlabs.com>
2025-04-13 13:41:57 -0400

    modifying codeserver-ext-jvm.sh script

--- codeserver/codeserver-ext-jvm.sh
+++ codeserver/codeserver-ext-jvm.sh
@@ -6,11 +6,6 @@ if command -v "code-server" > /dev/null 2>&1; then
 
     ## INSTALLING CODE SERVER EXTENSIONS [jvm]
     echo "[INFO] Installing code-server extensions [jvm]..."
-    code-server --install-extension redhat.java >> /var/log/vscode-ext.out 2>> /var/log/vscode-ext.err < /dev/null &
-    code-server --install-extension vscjava.vscode-java-test >> /var/log/vscode-ext.out 2>> /var/log/vscode-ext.err < /dev/null &
-    code-server --install-extension vscjava.vscode-java-debug >> /var/log/vscode-ext.out 2>> /var/log/vscode-ext.err < /dev/null &
-    code-server --install-extension vscjava.vscode-java-dependency >> /var/log/vscode-ext.out 2>> /var/log/vscode-ext.err < /dev/null &
-    code-server --install-extension vscjava.vscode-maven >> /var/log/vscode-ext.out 2>> /var/log/vscode-ext.err < /dev/null &
+    code-server --install-extension vscjava.vscode-java-pack >> /var/log/vscode-ext.out 2>> /var/log/vscode-ext.err < /dev/null &
     code-server --install-extension vmware.vscode-boot-dev-pack >> /var/log/vscode-ext.out 2>> /var/log/vscode-ext.err < /dev/null &
-    
 fi
