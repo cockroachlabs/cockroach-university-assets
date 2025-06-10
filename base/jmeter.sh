3af688313098a01b0ca6409d7d2824b9629ed2c2
Felipe Gutierrez <felipe.gutierrezcruz@cockroachlabs.com>
2025-06-10 10:06:20 -0400

    fixing base/jmeter script

--- base/jmeter.sh
+++ base/jmeter.sh
@@ -6,12 +6,12 @@ echo "Download Apache Jmeter for load testing"
 JMETER_VERSION=5.6.3
 POSTGRES_VERSION=42.7.3
 
-## Jmeter Directory
-
-# apt install -y jmeter
+## jmeter
+cd /tmp
 wget -q https://dlcdn.apache.org//jmeter/binaries/apache-jmeter-${JMETER_VERSION}.tgz
 tar -xzf apache-jmeter-${JMETER_VERSION}.tgz
-mv apache-jmeter-${JMETER_VERSION} /root/jmeter
+mv apache-jmeter-${JMETER_VERSION} jmeter
+mv jmeter /root/
 
 # download the PostgreSQL JDBC driver and install into the Jmeter lib directory
 wget -q -P /root/jmeter/lib https://jdbc.postgresql.org/download/postgresql-${POSTGRES_VERSION}.jar
