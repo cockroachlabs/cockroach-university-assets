a23bea96b945e1c77d55eee09c7331bb1b70151b
Felipe Gutierrez <felipe.gutierrezcruz@cockroachlabs.com>
2025-12-03 18:49:45 -0500

    adding some updates to the postgres.sh for the new migration-labs

--- courses/migration-labs/postgres.sh
+++ courses/migration-labs/postgres.sh
@@ -9,7 +9,11 @@ sed -i "s/#max_replication_slots = 10/max_replication_slots = 10/" /etc/postgres
 sed -i "s/shared_buffers = 128MB/shared_buffers = 512MB/" /etc/postgresql/*/main/postgresql.conf || true
 sed -i "s/max_connections = 100/max_connections = 200/" /etc/postgresql/*/main/postgresql.conf || true
 
-service postgresql start
+service postgresql restart
+
+echo "*:*:*:*:postgres" > ~/.pgpass
+chmod 600 ~/.pgpass
 
 # Set postgres user password
 sudo -u postgres psql -c "ALTER USER postgres PASSWORD 'postgres';"
+export PGPASSWORD=postgres
